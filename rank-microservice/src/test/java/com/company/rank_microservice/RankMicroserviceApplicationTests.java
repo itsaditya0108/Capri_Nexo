6ac27db3149b8fe7a2f45b753b4fefc7dbad3d23
Aditya <adityaverma90055@gmail.com>
2026-01-10 13:18:18 +0530

    Initial commit: user and rank microservices

---
+++ rank-microservice/src/test/java/com/company/rank_microservice/RankMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.rank_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RankMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
